3c1480038f416bb9b766552cefb81a07a262fb13
jario-jin <jariof@foxmail.com>
2019-04-02 16:09:24 +0800

    update stat

--- utils/annotation_stat.py
+++ utils/annotation_stat.py
@@ -20,15 +20,39 @@ def open_spire_annotations(spire_dir):
     return image_jsons
 
 
+def topdown_percent_wh(bboxes, percent=0.2):
+    """
+    :param bboxes: np.float32, [n, 4], x,y,w,h
+    :param percent: (0, 1]
+    :return: min_w, max_w, min_h, max_h
+    """
+    if bboxes.ndim == 1:
+        bboxes = np.expand_dims(bboxes, axis=0)
+
+    num_samp = int(np.ceil(len(bboxes) * percent))
+    width_ids = np.argsort(bboxes[:, 2])
+    height_ids = np.argsort(bboxes[:, 3])
+    min_w = bboxes[width_ids[:num_samp], 2]
+    min_w = np.mean(min_w)
+    max_w = bboxes[width_ids[-num_samp:], 2]
+    max_w = np.mean(max_w)
+    min_h = bboxes[height_ids[:num_samp], 3]
+    min_h = np.mean(min_h)
+    max_h = bboxes[height_ids[-num_samp:], 3]
+    max_h = np.mean(max_h)
+    return min_w, max_w, min_h, max_h
+
+
 def main():
     parser = argparse.ArgumentParser(description="Statistic on spire annotations")
     parser.add_argument(
         "--spire-anno",
         default="/tmp/coco_spire",
         help="path to spire annotation dir",
-        required=True
+        # required=True
     )
     args = parser.parse_args()
+    use_topdown_percent = True
 
     image_jsons = open_spire_annotations(args.spire_anno)
     image_width_min, image_height_min = np.inf, np.inf
@@ -46,15 +70,29 @@ def main():
         image_height_min = np.minimum(image_height_min, image_anno['height'])
         image_height_max = np.maximum(image_height_max, image_anno['height'])
 
+        bboxes = []
         for anno in image_anno['annos']:
-            bbox = anno['bbox']
+            bbox = np.array(anno['bbox'], np.float32)
+            bboxes.append(bbox)
             bbox_width, bbox_height = bbox[2], bbox[3]
-            bbox_width_min = np.minimum(bbox_width_min, bbox_width)
-            bbox_width_max = np.maximum(bbox_width_max, bbox_width)
-            bbox_height_min = np.minimum(bbox_height_min, bbox_height)
-            bbox_height_max = np.maximum(bbox_height_max, bbox_height)
+            if not use_topdown_percent:
+                bbox_width_min = np.minimum(bbox_width_min, bbox_width)
+                bbox_width_max = np.maximum(bbox_width_max, bbox_width)
+                bbox_height_min = np.minimum(bbox_height_min, bbox_height)
+                bbox_height_max = np.maximum(bbox_height_max, bbox_height)
             area_min = np.minimum(area_min, anno['area'])
             area_max = np.maximum(area_max, anno['area'])
+
+        # need at least one array to concatenate
+        if len(bboxes) > 0:
+            bboxes = np.vstack(bboxes)
+            min_w, max_w, min_h, max_h = topdown_percent_wh(bboxes)
+
+            if use_topdown_percent:
+                bbox_width_min = np.minimum(bbox_width_min, min_w)
+                bbox_width_max = np.maximum(bbox_width_max, max_w)
+                bbox_height_min = np.minimum(bbox_height_min, min_h)
+                bbox_height_max = np.maximum(bbox_height_max, max_h)
 
     print('image_width_min:{:0.2f}, image_height_min:{:0.2f}'.format(image_width_min, image_height_min))
     print('image_width_max:{:0.2f}, image_height_max:{:0.2f}'.format(image_width_max, image_height_max))
